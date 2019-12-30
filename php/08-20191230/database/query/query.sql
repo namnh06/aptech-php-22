59ed5e7a7fe8932ddbd41a8c510518368c286849
Nam H Nguyen <hainamdng@yahoo.com.vn>
2019-12-30 20:11:15 +0700

    update php8

---
+++ php/08-20191230/database/query/query.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE aptech_php_22_08;
+CREATE TABLE aptech_php_22_08.users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255) UNIQUE
+);
+INSERT INTO aptech_php_22_08.users (name,email)
+VALUES
+('nam','nam@gmail.com'),
+('nam1','nam1@gmail.com'),
+('nam2','nam2@gmail.com'),
+('nam3','nam3@gmail.com'),
+('nam4','nam4@gmail.com');
+
+SELECT * FROM aptech_php_22_08.users;
