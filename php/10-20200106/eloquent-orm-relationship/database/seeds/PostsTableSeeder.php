cee31c3afa9d175f436090a03d2f4ea65322cc71
Nam H Nguyen <hainamdng@yahoo.com.vn>
2020-01-06 19:25:04 +0700

    Update php10

---
+++ php/10-20200106/eloquent-orm-relationship/database/seeds/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        for ($i = 0; $i < 10; $i++) {
+            $title = $faker->sentence($nbWords = 6, $variableNbWords = true);
+            DB::table('posts')->insert([
+                'title' => $title,
+                'slug' => str_replace(" ", "-", $title),
+                'description' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+                'content' => $faker->paragraph($nbSentences = 10, $variableNbSentences = true)
+            ]);
+        }
+    }
+}
